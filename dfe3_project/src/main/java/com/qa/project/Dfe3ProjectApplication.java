c9f6b71b34f746c7a6d980be81dfea4b9b919aa9
David Cull <nowe2111@yahoo.co.uk>
2021-11-14 04:10:40 +0000

    hopeful commit, no test due to computer issues

---
+++ dfe3_project/src/main/java/com/qa/project/Dfe3ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dfe3ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dfe3ProjectApplication.class, args);
+	}
+
+}
